469897ffe3b2fc2d1ad597e1fd244b581770c07c
NikulinIlya <ilia.nikulin@gmail.com>
2020-05-21 18:29:05 +0300

    feat(JokeFactoryTest): make it_returns_a_predefined_joke

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -20,4 +20,20 @@ class JokeFactoryTest extends TestCase
 
         $this->assertSame('This is a joke', $joke);
     }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+            'Chuck Norris counted to infinity... Twice.',
+            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
+        ];
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
 }
