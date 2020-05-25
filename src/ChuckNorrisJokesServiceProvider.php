a212a5b067cc3585d60dc25ec9385bbf60167edc
Ilya Nikulin <ilia.nikulin@gmail.com>
2020-05-25 17:05:55 +0000

    Apply fixes from StyleCI

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -17,22 +17,22 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
             ]);
         }
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'chuck-norris');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chuck-norris');
 
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/chuck-norris'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/chuck-norris'),
         ], 'views');
 
         $this->publishes([
-            __DIR__ . '/../config/chuck-norris.php' => base_path('config/chuck-norris.php'),
+            __DIR__.'/../config/chuck-norris.php' => base_path('config/chuck-norris.php'),
         ], 'config');
 
         if (! class_exists('CreateJokesTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_jokes_table.php.stub' => database_path(
+                __DIR__.'/../database/migrations/create_jokes_table.php.stub' => database_path(
                     'migrations/'
-                    . date('Y_m_d_His', time())
-                    . '_create_jokes_table.php'
+                    .date('Y_m_d_His', time())
+                    .'_create_jokes_table.php'
                 ),
             ], 'migrations');
         }
@@ -46,6 +46,6 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
             return new JokeFactory();
         });
 
-        $this->mergeConfigFrom(__DIR__ . '/../config/chuck-norris.php', 'chuck-norris');
+        $this->mergeConfigFrom(__DIR__.'/../config/chuck-norris.php', 'chuck-norris');
     }
 }
